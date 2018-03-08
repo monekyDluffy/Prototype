ecf940a1011fc47683d4cf9fd7074de85ffb11a2
yusheng <yusheng.liu@lianbi.com.cn>
2018-03-08 15:37:53 +0800

    Initial Commit

---
+++ Prototype/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Prototype
+//
+//  Created by yusheng on 2018/3/8.
+//  Copyright © 2018年 yusheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
